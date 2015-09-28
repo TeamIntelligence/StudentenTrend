934a647086235228ee900bfac470408a40075f32
Ramon Esteve Cuevas <ramonestevecuevas@gmail.com>
2015-09-28 11:42:02 +0200

    Added endpoint (sector_naam) to bedrijfstak

--- src/main/java/studententrend/controller/BedrijfsTakController.java
+++ src/main/java/studententrend/controller/BedrijfsTakController.java
@@ -3,6 +3,7 @@ package studententrend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,10 +25,32 @@ public class BedrijfsTakController {
 			produces = {"application/json"})
 	@ResponseBody
 	public List<BedrijfsTak> bedrijfsTak() {
-		
 		List<BedrijfsTak> bedrijfsTakken = (List<BedrijfsTak>) bedrijfsTakRepository.findAll();
+		
 		for(BedrijfsTak bedrijfsTak : bedrijfsTakken){
-			System.out.println(bedrijfsTak.toString());
+			System.out.println(bedrijfsTak.toString() + '\n');
+		}
+		
+		return bedrijfsTakken;
+	}
+	
+	@RequestMapping(
+			value = "/bedrijfstak/{sector_naam}", 
+			method = RequestMethod.GET,
+			headers = "Accept=application/json", 
+			produces = {"application/json"})
+	@ResponseBody
+	public List<BedrijfsTak> bedrijfsTak(@PathVariable String sector_naam) {
+		List<BedrijfsTak> bedrijfsTakken = null;
+		
+		if(!sector_naam.isEmpty()) {
+			bedrijfsTakken = (List<BedrijfsTak>) bedrijfsTakRepository.findBySectorNaam(sector_naam);
+		} else {
+			bedrijfsTakken = bedrijfsTak();
+		}
+		
+		for(BedrijfsTak bedrijfsTak : bedrijfsTakken){
+			System.out.println(bedrijfsTak.toString() + '\n');
 		}
 		
 		return bedrijfsTakken;
--- src/main/java/studententrend/model/dao/BedrijfsTakRepository.java
+++ src/main/java/studententrend/model/dao/BedrijfsTakRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.repository.CrudRepository;
 import studententrend.model.BedrijfsTak;
 
 public interface BedrijfsTakRepository extends CrudRepository<BedrijfsTak, Long> {
+	List<BedrijfsTak> findBySectorCode(String sector_id);
 	List<BedrijfsTak> findBySectorNaam(String sectorNaam);
 }
