d43446f052d5aeb59947d3b4db318db8804a06a5
Anne-Marie Smit <anne@smit.cc>
2015-11-23 14:18:42 +0100

    Extra knoppen bij plot gemaakt

--- StudentenTrendShiny/screens/StudentenIngeschreven.R
+++ StudentenTrendShiny/screens/StudentenIngeschreven.R
@@ -14,12 +14,25 @@ StudentenIngeschrevenUI <- function(PageName){
                             selected = 1
                 ),
                 
+                checkboxInput("Alles",
+                              "Geef alle studies weer"
+                ),
+                
+                checkboxInput("Totaalselect",
+                              "Totaal lijn weergeven van de geselecteerde studies"
+                ),
+                
+                checkboxInput("Totaal",
+                              "Totaal lijn weergeven"
+                ),
+                
                 radioButtons("StudieNiveau",
-                             "Soort", 
+                             "Studie Niveau", 
                              choices = list("HBO" = "HBO", 
                                             "WO"  = "WO",
                                             "HBO en WO" = "HBOWO")
                 )
+              
               ),
               
               # Show a plot of the generated distribution
@@ -28,9 +41,7 @@ StudentenIngeschrevenUI <- function(PageName){
                 
               )
               
-              
             )
-            
             
     )
   )
@@ -40,20 +51,24 @@ StudentenIngeschrevenServer <- function(input, output){
   
   output$aantalIngeschrevenPlot <- renderPlot({
     
+    #data aanpassen nav keuzes gebruiker
     siSub <- switch (input$StudieNiveau,
                      "HBO" = studenten_ingeschrevenen[studenten_ingeschrevenen$ondCode == "HBO",],
                      "WO" = studenten_ingeschrevenen[studenten_ingeschrevenen$ondCode == "WO",],
                      "HBOWO" = aggregate(studenten_ingeschrevenen$aantal, by=list(iscedNaam=studenten_ingeschrevenen$iscedCode.iscedNaam,jaartal=studenten_ingeschrevenen$jaartal), FUN=sum)
     )
     
+    #namen kolomtitels van de nieuwe gevormde data aanpassen
     if(input$StudieNiveau == "HBOWO"){
       colnames(siSub)<-c("iscedCode.iscedNaam","jaartal","aantal")
     }
     
+    if(input$Alles == FALSE){ #als je niet alles wilt, alleen dan kijken naar de gekozen studies
+      siSub <- siSub[siSub$iscedCode.iscedNaam %in% input$SelectStudy,]
+    }
     
-    siSub <- siSub[siSub$iscedCode.iscedNaam %in% input$SelectStudy,]
-    
-    if (!is.null(input$SelectStudy)){ #minimaal 1 studie
+    #plotten en titel laten afhangen
+    if ( (!is.null(input$SelectStudy)) | (input$Alles==TRUE) ){ #minimaal 1 studie, of alle studies
       if(length(input$SelectStudy) == 1){ #1studie
         plotTitle <- paste("Aantal ingeschreven bachelor studenten \nper jaar verdeeld per studie", input$SelectStudy)
       } 
@@ -67,17 +82,47 @@ StudentenIngeschrevenServer <- function(input, output){
         
       }
       
-      ggplot(siSub, 
-             aes(x=jaartal, y=aantal, 
-                 group=iscedCode.iscedNaam,
-                 color=iscedCode.iscedNaam)) + 
-        xlab("Jaar") +  
-        ylab("Aantal studenten") + 
-        ggtitle(plotTitle) +
-        geom_line() + 
-        geom_point() +
-        labs(color = "Studierichting") 
+#       if(input$totaal == FALSE){
+        
+        ggplot(siSub, 
+               aes(x=jaartal)) + 
+          xlab("Jaar") +  
+          ylab("Aantal studenten") + 
+          ggtitle(plotTitle) +
+          geom_line(aes(y=aantal, 
+                        group=iscedCode.iscedNaam,
+                        color=iscedCode.iscedNaam)) + 
+          geom_point(aes(y=aantal, 
+                         group=iscedCode.iscedNaam,
+                         color=iscedCode.iscedNaam)) +
+          labs(color = "Studierichting") 
+        
+#       }
+#       else{
+#         totaalaantal <- aggregate(studenten_ingeschrevenen$aantal, by=list(ondCode=studenten_ingeschrevenen$ondCode,jaartal=studenten_ingeschrevenen$jaartal), FUN=sum)
+#         
+#         ggplot(siSub, 
+#                aes(x=jaartal)) + 
+#           xlab("Jaar") +  
+#           ylab("Aantal studenten") + 
+#           ggtitle(plotTitle) +
+#           geom_line(aes(y=aantal, 
+#                         group=iscedCode.iscedNaam,
+#                         color=iscedCode.iscedNaam)) + 
+#           geom_point(aes(y=aantal, 
+#                          group=iscedCode.iscedNaam,
+#                          color=iscedCode.iscedNaam)) +
+#           geom_line(aes(y=totaalaantal, 
+#                         group=ondCode,
+#                         color=ondCode)) + 
+#           geom_point(aes(y=totaalaantal, 
+#                          group=ondCode,
+#                          color=ondCode)) +
+#           labs(color = "Studierichting") 
+#       }
+      
     } 
+    
   })
   
 }
