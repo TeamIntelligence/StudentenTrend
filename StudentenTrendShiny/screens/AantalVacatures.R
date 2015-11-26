cb84ba7f398dd1de2442ab16200b08a5cc3e6fb1
Ilsa Brooijmans <ilsabrooijmans@hotmail.com>
2015-11-26 12:07:52 +0100

    Gediplomeerden en vacatures aangepast. Twee plotjes nu.

--- StudentenTrendShiny/screens/AantalVacatures.R
+++ StudentenTrendShiny/screens/AantalVacatures.R
@@ -5,7 +5,7 @@ AantalVacaturesUI <- function(PageName){
             fluidRow(
             # Application title
               titlePanel("Aantal vacatures"),
-              box(width=4, height = "100%",
+              box(width=5, height = 150,
                   
                   selectInput("AantalVacatures_Select",
                               "Selecteer een of meerdere bedrijfssectoren om weer te geven:",
@@ -19,17 +19,19 @@ AantalVacaturesUI <- function(PageName){
                                 "Geef alle bedrijfssectoren weer"
                   )
                   
-#                   checkboxInput("AantalVacatures_TotaalGeselecteerd",
-#                                 "Totaal lijn weergeven van de geselecteerde bedrijfssectoren"
-#                   ),
-#                   
-#                   checkboxInput("AantalVacatures_Totaal",
-#                                 "Totaal lijn weergeven"
-#                   ),
+              ),
+              box(width=7, height=150,
+                  checkboxInput("AantalVacatures_TotaalGeselecteerd",
+                                "Totaal lijn weergeven van de geselecteerde bedrijfssectoren"
+                  ),
                   
+                  checkboxInput("AantalVacatures_Totaal",
+                                "Totaal lijn weergeven"
+                  )
                   
               )
-              ,box(width=8, height = 600, plotOutput("VacaPlot", height=600))
+              ,box(width=5, height = 470, plotOutput("VacaPlot", height = 450))
+              ,box(width=7, height = 470, plotOutput("VacaBarPlot", height=450))
             )  
             
     )
@@ -40,12 +42,12 @@ AantalVacaturesServer <- function(input,output){
   output$VacaPlot <- renderPlot({
     
     
-    AantalVacatures_vacSub <<- vacatures[vacatures$sbiCode.sbiNaam %in% input$AantalVacatures_Select,]
     
+    AantalVacatures_vacSub <<- vacatures_jaartallen
     
     
     if(input$AantalVacatures_AlleSectoren == FALSE){ #als je niet alles wilt, alleen dan kijken naar de gekozen vacatures
-      AantalVacatures_vacSub <- AantalVacatures_vacSub[AantalVacatures_vacSub$sbiCode.sbiNaam %in% input$AantalVacatures_Select,]
+      AantalVacatures_vacSub <<- vacatures_jaartallen[vacatures_jaartallen$sbiCode.sbiNaam %in% input$AantalVacatures_Select,]
     }
     
     #plotten en titel laten afhangen
@@ -69,9 +71,45 @@ AantalVacaturesServer <- function(input,output){
       xlab("Jaar") + 
       ylab("Aantal vacatures") +
       ggtitle(plotTitle) +
-      geom_line(aes(y=aantal, group=sbiCode.sbiNaam, color=sbiCode.sbiNaam)) + 
-      geom_point(aes(y=aantal, group=sbiCode.sbiNaam, color=sbiCode.sbiNaam)) +
-      labs(color = "Bedrijfssectoren") 
+      geom_line(aes(y=aantal, color=sbiCode.sbiNaam))+
+      geom_point(aes(y=aantal, color=sbiCode.sbiNaam))+ 
+      theme(legend.position="none")
+    }
+    
+  })
+  output$VacaBarPlot <- renderPlot({
+    
+    
+    
+    AantalVacatures_vacBarSub <<- vacatures_jaartallen
+    
+    
+    if(input$AantalVacatures_AlleSectoren == FALSE){ #als je niet alles wilt, alleen dan kijken naar de gekozen vacatures
+      AantalVacatures_vacBarSub <<- vacatures_jaartallen[vacatures_jaartallen$sbiCode.sbiNaam %in% input$AantalVacatures_Select,]
+    }
+    
+    #plotten en titel laten afhangen
+    if ( (!is.null(input$AantalVacatures_Select)) | (input$AantalVacatures_AlleSectoren==TRUE) ){ #minimaal 1 vacature, of alle vacatures
+      if(length(input$AantalVacatures_Select) == 1){ #1vacature
+        plotTitle <- paste("Aantal vacatures \nper jaar verdeeld per bedrijfssector", input$AantalVacatures_Select)
+      } 
+      else{ # meerdere vacatures, met namen in de titel
+        names     <- paste(input$AantalVacatures_Select, collapse = ', ')
+        plotTitle <- paste("Aantal vacatures voor:", names)
+        
+        if (nchar(plotTitle) > 100){ #te lange naam aanpassen
+          plotTitle <- "Aantal vacatures voor verscheidene bedrijfssectoren"
+        }
+        
+      }
+      
+      # draw the histogram
+      ggplot(AantalVacatures_vacBarSub, aes(x=jaartal)) +
+        xlab("Jaar") + 
+        ylab("Aantal vacatures") +
+        ggtitle(plotTitle) +
+        geom_bar(stat = "identity", aes(y=aantal, fill=sbiCode.sbiNaam)) + 
+        labs(fill = "Bedrijfssectoren") 
     }
     
   })
--- StudentenTrendShiny/screens/StudentenGediplomeerden.R
+++ StudentenTrendShiny/screens/StudentenGediplomeerden.R
@@ -1,11 +1,11 @@
 StudentenGediplomeerdenUI <- function(PageName){
   return(
     
-    tabItem(tabName = PageName,
+    tabItem(tabName = PageName, 
             fluidRow(
               # Application title
               titlePanel("Gediplomeerde studenten"),
-              box(width=4, height = "100%",
+              box(width=4, height = 150, #background = "maroon",
                   
                   selectInput("StudentenGediplomeerden_StudySelect",
                               "Selecteer een of meerdere studiesectoren om weer te geven:",
@@ -17,33 +17,39 @@ StudentenGediplomeerdenUI <- function(PageName){
                   
                   checkboxInput("StudentenGediplomeerden_AlleStudies",
                                 "Geef alle studies weer"
+                  )
+              ),
+              box(width=4, height = 150,
+                radioButtons("StudentenGediplomeerden_StudieNiveau",
+                             "Studie Niveau", 
+                             choices = list("HBO" = "HBO", 
+                                            "WO Bachelor"  = "WOB",
+                                            "WO Master" = "WOM",
+                                            "HBO en WO Master" = "HBOWO")
+                )
+              ),
+              
+              box(width=4, height = 150,
+                  
+                  checkboxInput("StudentenGediplomeerden_TotaalGeselecteerd",
+                                "Totaal lijn weergeven van de geselecteerde studies"
                   ),
                   
-#                   checkboxInput("StudentenGediplomeerden_TotaalGeselecteerd",
-#                                 "Totaal lijn weergeven van de geselecteerde studies"
-#                   ),
-#                   
-#                   checkboxInput("StudentenGediplomeerden_Totaal",
-#                                 "Totaal lijn weergeven"
-#                   ),
-                  
-                  radioButtons("StudentenGediplomeerden_StudieNiveau",
-                               "Studie Niveau", 
-                               choices = list("HBO" = "HBO", 
-                                              "WO Bachelor"  = "WOB",
-                                              "WO Master" = "WOM",
-                                              "HBO en WO Master" = "HBOWO")
+                  checkboxInput("StudentenGediplomeerden_Totaal",
+                                "Totaal lijn weergeven"
                   )
-                  
               )
-              ,box(width=8, height = 600, plotOutput("DiploPlot", height=600))
+                  
+            
+            ,box(width=5, height = 470, plotOutput("DiploPlot", height=450))
+            ,box(width=7, height = 470, plotOutput("DiploBarPlot", height=450))
             )
+          )
     )
-  )
 
 }
 
-StudentenGediplomeerdenServer <- function(input, output, session){
+StudentenGediplomeerdenServer <- function(input, output){
   output$DiploPlot <- renderPlot({
     HWSet <<- studenten_gediplomeerden[(studenten_gediplomeerden$ondCode == "HBO" & studenten_gediplomeerden$diploma == "Bachelor") | (studenten_gediplomeerden$ondCode == "WO" & studenten_gediplomeerden$diploma == "Wo-master"),] 
     
@@ -86,65 +92,60 @@ StudentenGediplomeerdenServer <- function(input, output, session){
         xlab("Jaar") +  
         ylab("Aantal studenten") + 
         ggtitle(plotTitle) +
-        geom_line(aes(y=aantal, group=iscedCode.iscedNaam, color=iscedCode.iscedNaam)) + 
-        geom_point(aes(y=aantal, group=iscedCode.iscedNaam, color=iscedCode.iscedNaam)) +
-        labs(color = "Studierichting") 
-    
-    
-    
-#     soortSub <- switch (input$StudieSoort,
-#                      "HBO" = studenten_gediplomeerden[studenten_gediplomeerden$ondCode == "HBO",],
-#                      "WO" = studenten_gediplomeerden[studenten_gediplomeerden$ondCode == "WO",],
-#                      "HBOWO" = aggregate(studenten_gediplomeerden$aantal, by=list(iscedNaam=studenten_gediplomeerden$iscedCode.iscedNaam,jaartal=studenten_gediplomeerden$jaartal), FUN=sum)
-#     )
-#     
-#     if(input$StudieSoort == "HBOWO"){
-#       colnames(soortSub)<-c("iscedCode.iscedNaam","jaartal","aantal")
-#     }
-#     
-#      observe({
-#        if (input$Uncheck > 0){
-#          updateCheckboxGroupInput(session=session,inputId="checkGroup", choices=unique(studenten_gediplomeerden$iscedCode.iscedNaam), selected=NULL)
-#          
-#          }
-#      })
-#     
-#     soortSub <- soortSub[soortSub$iscedCode.iscedNaam %in% input$checkGroup,]
-#     
-#     
-#     if (!is.null(input$checkGroup)){ #minimaal 1 studie
-#       if(length(input$checkGroup) == 1){ #1studie
-#         plotTitle <- paste("Aantal afgestudeerden studenten \nper jaar verdeeld per studie", input$checkGroup)
-#       } 
-#       else{ # meerdere studies, met namen in de titel
-#         names     <- paste(input$checkGroup, collapse = ', ')
-#         plotTitle <- paste("Aantal afgestudeerden studenten voor:", names)
-#         
-#         if (nchar(plotTitle) > 100){ #te lange naam aanpassen
-#           plotTitle <- "Aantal afgestudeerden studenten voor verscheidene opleidingen"
-#         }
-#         
-#       }
-#       
-#       ggplot(soortSub, aes(x=jaartal, y=aantal, fill=iscedCode.iscedNaam),environment = environment())+
-#         xlab("Jaar") +  
-#         ylab("Aantal gediplomeerde studenten") + 
-#         ggtitle(plotTitle) +
-#         geom_bar(stat = "identity")+
-#         #labs(color = "Studierichting")
-#         scale_fill_manual(values=rainbow(length(input$checkGroup)),name="Studierichting")
-#       
-#       ggplot(soortSub, aes(x=jaartal, y=aantal, group=iscedCode.iscedNaam,color=iscedCode.iscedNaam)) + 
-#         xlab("Jaar") +  
-#         ylab("Aantal gediplomeerde studenten") + 
-#         ggtitle(plotTitle) +
-#         geom_line() + 
-#         geom_point() +
-#         labs(color = "Studierichting")
-      
-      } 
+        geom_line(aes(y=aantal, color=iscedCode.iscedNaam)) + 
+        geom_point(aes(y=aantal, color=iscedCode.iscedNaam)) +
+        labs(color = "Studierichting")+ 
+        theme(legend.position="none")
+    } 
   })
   
+  output$DiploBarPlot <- renderPlot({
+    HWBarSet <<- studenten_gediplomeerden[(studenten_gediplomeerden$ondCode == "HBO" & studenten_gediplomeerden$diploma == "Bachelor") | (studenten_gediplomeerden$ondCode == "WO" & studenten_gediplomeerden$diploma == "Wo-master"),] 
+    
+    
+    #data aanpassen nav keuzes gebruiker
+    StudentenGediplomeerden_StudieBarSub <- switch (input$StudentenGediplomeerden_StudieNiveau,
+                                                 "HBO" = studenten_gediplomeerden[studenten_gediplomeerden$ondCode == "HBO" & studenten_gediplomeerden$diploma == "Bachelor",] ,
+                                                 "WOB" = studenten_gediplomeerden[studenten_gediplomeerden$ondCode == "WO" & studenten_gediplomeerden$diploma == "Bachelor",],
+                                                 "WOM"= studenten_gediplomeerden[studenten_gediplomeerden$ondCode == "WO" & studenten_gediplomeerden$diploma == "Wo-master",],
+                                                 "HBOWO" = aggregate(HWBarSet$aantal, by=list(iscedNaam=HWBarSet$iscedCode.iscedNaam, jaartal=HWBarSet$jaartal), FUN=sum)
+    )
+    
+    
+    #namen kolomtitels van de nieuwe gevormde data aanpassen
+    if(input$StudentenGediplomeerden_StudieNiveau == "HBOWO"){
+      colnames(StudentenGediplomeerden_StudieBarSub)<-c("iscedCode.iscedNaam","jaartal","aantal")
+    }
+    
+    if(input$StudentenGediplomeerden_AlleStudies == FALSE){ #als je niet alles wilt, alleen dan kijken naar de gekozen studies
+      StudentenGediplomeerden_StudieBarSub <- StudentenGediplomeerden_StudieBarSub[StudentenGediplomeerden_StudieBarSub$iscedCode.iscedNaam %in% input$StudentenGediplomeerden_StudySelect,]
+    }
+    
+    #plotten en titel laten afhangen
+    if ( (!is.null(input$StudentenGediplomeerden_StudySelect)) | (input$StudentenGediplomeerden_AlleStudies==TRUE) ){ #minimaal 1 studie, of alle studies
+      if(length(input$StudentenGediplomeerden_StudySelect) == 1){ #1studie
+        plotTitle <- paste("Aantal gediplomeerde bachelor studenten \nper jaar verdeeld per studie", input$StudentenGediplomeerden_StudySelect)
+      } 
+      else{ # meerdere studies, met namen in de titel
+        names     <- paste(input$StudentenGediplomeerden_StudySelect, collapse = ', ')
+        plotTitle <- paste("Aantal gediplomeerde bachelor studenten voor:", names)
+        
+        if (nchar(plotTitle) > 100){ #te lange naam aanpassen
+          plotTitle <- "Aantal gediplomeerde bachelor studenten voor verscheidene opleidingen"
+        }
+        
+      }
+      
+      
+      ggplot(StudentenGediplomeerden_StudieBarSub, aes(x=jaartal)) + 
+        xlab("Jaar") +  
+        ylab("Aantal studenten") + 
+        ggtitle(plotTitle) +
+        geom_bar(stat = "identity", aes(y=aantal, fill=iscedCode.iscedNaam)) + 
+        labs(color = "Studierichting")
 
+    } 
+  })
   
-}
+}  
+
