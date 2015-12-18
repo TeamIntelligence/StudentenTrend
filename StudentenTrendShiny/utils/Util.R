08d943e3ed797d3bfcf3d61149ec91443a24b18f
Ramon Esteve Cuevas <ramonestevecuevas@gmail.com>
2015-12-18 09:40:42 +0100

    Fixed the bug when trying to call a server function of NULL

--- StudentenTrendShiny/utils/Util.R
+++ StudentenTrendShiny/utils/Util.R
@@ -1,51 +1,50 @@
 GetPageNameSubItem <- function(SubItem) {
-  succeeded <- FALSE
   
   # Try to get the SubItemName 
   tryCatch({
-    succeeded <- TRUE
-    return(SubItem[[1]]["children"][["children"]][[1]]["attribs"][[1]][["data-value"]])
+    if(!is.null(SubItem[[1]]["children"][["children"]][[1]]["attribs"][[1]][["data-value"]])) {
+      return(SubItem[[1]]["children"][["children"]][[1]]["attribs"][[1]][["data-value"]])
+    }
   }
-  ,error = function(cond) {})
+  ,error = function(cond) {}
+  )
   
-  if(!succeeded) {
-    return("")
-  }
+  return("")
 }
 
 CallUIFunction <- function(Page) {
-  FunctionName <- paste(Page["tabName"], "UI", sep="")
-  
-  #Try to call the UI function, if not exist log it
-  tryCatch({
-    Item <- do.call(FunctionName, list(Page["tabName"]) ) 
-  }
-  ,error = function(cond) {
-    Item <<- tabItem(tabName = Page["tabName"],
-                     fluidRow(
-                       box(width=12, height = 800, title = paste("De ", Page["tabName"], " pagina is niet gevonden!")
-                       )
-                     )
-    )
+  if(!is.null(Page[["tabName"]])) {
+    FunctionName <- paste(Page["tabName"], "UI", sep="")
     
-    message(paste("Could not find the UI function of: ", Page["tabName"]))
-  })
-  
-  if(is.null(Item)) {
-    Item <- get("Item", envir = .GlobalEnv)
-    remove("Item", envir = .GlobalEnv)
+    #Try to call the UI function, if not exist log it
+    if(exists(FunctionName, envir=.GlobalEnv)) {
+      Item <- do.call(FunctionName, list(Page["tabName"]) ) 
+    } else {
+      Item <- tabItem(tabName = Page["tabName"],
+                      fluidRow(
+                        box(width=12, height = 800, title = paste("De ", Page["tabName"], " pagina is niet gevonden!")
+                        )
+                      )
+      )
+      
+      message(paste("Could not find the UI function of: ", Page["tabName"]))
+    }
+    
+    return(Item)
   }
-  
-  return(Item)
 }
 
 CallServerFunction <- function(Page, ...) {
-  FunctionName <- paste(Page["tabName"], "Server", sep="")
-  
-  #Try to call the server function, if not exist log it
-  if(exists(FunctionName, envir=.GlobalEnv)) {
-    do.call(FunctionName, list(...) ) 
-  } else {
-    message(paste("Could not find the Server function of: ", Page["tabName"]))
+  if(!is.null(Page[["tabName"]]) ) {
+    FunctionName <- paste(Page["tabName"], "Server", sep="")
+    
+    #Try to call the server function, if not exist log it
+    if(exists(FunctionName, envir=.GlobalEnv)) {
+      do.call(FunctionName, list(...) ) 
+    } else {
+      message(paste("Could not find the Server function of: ", Page["tabName"]))
+    }
   }
+  
+  Test <<- Page
 }
